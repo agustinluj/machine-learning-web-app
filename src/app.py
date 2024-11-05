83268c482d95f99a9ef7e35f0bc9af7c2ecf4676
Agus <lujan.agustin@gmail.com>
2024-11-05 19:03:36 +0100

    utimos

--- src/app.py
+++ src/app.py
@@ -2,3 +2,18 @@ from utils import db_connect
 engine = db_connect()
 
 # your code here
+from fastapi import FastAPI, HTTPException
+
+app = FastAPI()
+
+@app.get('/elevar_cuadrado')
+async def get_cuadrado(numero):
+    try:
+        numero = int(numero)
+    except:
+        raise: HTTPException(status_code=400, detail='Debes proporcionar un numero')
+    else:
+        return f'Resultado: {numero**2}'
+
+
+    
